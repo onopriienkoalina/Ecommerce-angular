5409746ddee694f30b026a2c4296b0225b68b07c
Alina Onopriienko <onopriienko.alina@gmail.com>
2026-06-19 13:38:10 +0300

    task 3.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,4 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
 }
--- src/app/features/products/product.ts
+++ src/app/features/products/product.ts
@@ -1,9 +1,15 @@
+export type DiscountTheme = 70 | 60 | 50;
 export interface Product {
   id: number;
   title: string;
+  description: string;
   price: number;
-  oldPrice?: number;
-  discount?: string;
+  oldPrice: number;
   imageUrl: string;
   rating: string;
+}
+export interface ProductCard extends Product {
+  discountPercent: number;
+  discountTheme: DiscountTheme;
+  isMain: boolean;
 }
