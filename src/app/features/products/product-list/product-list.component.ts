412737046cd46bbfc0d4c6f63cfef63ffce3937b
onopriienkoalina <93276483+onopriienkoalina@users.noreply.github.com>
2026-07-08 18:55:11 +0300

    task 3.4

--- src/app/features/products/product-list/product-list.component.ts
+++ src/app/features/products/product-list/product-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, input } from '@angular/core';
 import type { ProductCard } from '../product';
-import {ProductCardComponent} from '../product-card/product-card.component';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-product-list',
   imports: [ProductCardComponent],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
   readonly products = input.required<ProductCard[]>();
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductListComponent } from '../../features/products/product-list/product-list.component';
 import { ProductsService } from '../../features/products/products.service';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -6,9 +6,9 @@ import { toSignal } from '@angular/core/rxjs-interop';
   selector: 'app-main-page',
   imports: [ProductListComponent],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
+  styleUrl: './main-page.component.css',
 })
 export class MainPageComponent {
   protected readonly productsService = inject(ProductsService);
-  protected readonly products = toSignal(this.productsService.getProducts(), { initialValue: []});
+  protected readonly products = toSignal(this.productsService.getProducts(), { initialValue: [] });
 }
